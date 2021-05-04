ea1ef7b5eacd666956eec5c500a7b54acc56202a
Greathoney <thyoon48@gmail.com>
2021-05-04 15:21:22 +0900

    commited

--- mediapipe-example/hands-webcam.py
+++ mediapipe-example/hands-webcam.py
@@ -56,13 +56,13 @@ with mp_hands.Hands(
     #   print(0)
 
     # Draw the hand annotations on the image.
-    # image.flags.writeable = True
-    # image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    # if results.multi_hand_landmarks:
-    #   for hand_landmarks in results.multi_hand_landmarks:
-    #     mp_drawing.draw_landmarks(
-    #         image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
-    # cv2.imshow('MediaPipe Hands', image)
-    # if cv2.waitKey(5) & 0xFF == 27:
-    #   break
+    image.flags.writeable = True
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    if results.multi_hand_landmarks:
+      for hand_landmarks in results.multi_hand_landmarks:
+        mp_drawing.draw_landmarks(
+            image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+    cv2.imshow('MediaPipe Hands', image)
+    if cv2.waitKey(5) & 0xFF == 27:
+      break
 cap.release()
